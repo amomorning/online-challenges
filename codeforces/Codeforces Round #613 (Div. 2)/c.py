a68057ce51d52021461aa7e9d22e13dc425eb8d1
Yichen Mo <amomorning@gmail.com>
2020-01-12 19:16:52 +0800

    Update c.py

--- codeforces/Codeforces Round #613 (Div. 2)/c.py
+++ codeforces/Codeforces Round #613 (Div. 2)/c.py
@@ -1,15 +1,18 @@
 import math
 
 def lcm(a, b):
+    # print(a, b)
+    # print(math.gcd(a, b))
+    # print(a*b//math.gcd(a, b))
     return a*b//math.gcd(a, b)
 
 
 x = int(input())
 n = math.ceil(math.sqrt(x))
 
-ans = 0
+ans = 1
 for i in range(1, n):
-    if(x % i == 0 and lcm(x//i, x) == x):
+    if(x % i == 0 and lcm(x//i, i) == x):
         ans = i 
 
 print(ans, x//ans)
