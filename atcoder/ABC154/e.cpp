ac7f2e4d41899049921c28b5c92a214307cf596b
Yichen Mo <amomorning@gmail.com>
2020-02-10 00:21:55 +0800

    AtCoder Beginner Contest 154

--- atcoder/ABC154/e.cpp
+++ atcoder/ABC154/e.cpp
@@ -1,37 +1,31 @@
 #include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
 
-int a[111];
-int dp[111][5][2];
-
-std::string s;
-int k, N;
-
-int nR(int n, int r) {
-    if(n > N) return 0;
-    if(r == 1) return n;
-    if(r == 2) return n * (n - 1) / 2;
-    if(r == 3) return n * (n - 1) * (n - 2) / 6;
-    return 0;
-}
-
-long long dfs(int i, int st, int op) {
-    if(i == N) {
-        if(st == 0) return 1;
-        else return 0;
-    }
-    if(st == 0) return 1;
-
-    if(op) return nR(N-i, st) * pow(9, st);
-    else {
-        return dfs(i+1, k, 1)
-                + dfs(i+1, k-1, 1) * (s[i] - '1')
-                + dfs(i+1, k-1, 0);
-    }
-}
+string s;
+int n, k;
+ll dp[101][5][2];
 int main() {
-    std::cin >> s >> k;
-    N = s.size();
-    std::cout << dfs(0,k,0) << std::endl;
+    cin >> s >> k;
+    n = s.size();
+    dp[0][0][0] = 1; 
 
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= k; j++) {
+            // op = 0 = 
+            if(s[i] == '0') dp[i+1][j][0] += dp[i][j][0];
+            else {
+                dp[i+1][j][1] += dp[i][j][0];
+                dp[i+1][j+1][0] += dp[i][j][0];
+                dp[i+1][j+1][1] += dp[i][j][0]*(s[i]-'1');
+            }
+
+            // op = 1 <
+            dp[i+1][j][1] += dp[i][j][1];
+            dp[i+1][j+1][1] += dp[i][j][1] * 9;
+        }
+    }
+
+    cout << dp[n][k][0] + dp[n][k][1] << endl;
     int amo; std::cin >> amo;
 }
