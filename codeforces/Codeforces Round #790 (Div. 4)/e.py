0237490af3776a36cc68d4a5484dd610022d5360
Yichen Mo <amomorning@gmail.com>
2022-05-11 21:49:33 +0800

    Add h

--- codeforces/Codeforces Round #790 (Div. 4)/e.py
+++ codeforces/Codeforces Round #790 (Div. 4)/e.py
@@ -11,6 +11,8 @@ def printf(a):
     else:
         print(a)
 
+import bisect
+
 for _ in range(int(input())):
     n, q = map(int, input().split())
     a = list(map(int, input().split()))
@@ -24,16 +26,7 @@ for _ in range(int(input())):
     for i in range(q):
         x = int(input())
 
-        l, r = 0, n
-
-        while(l < r):
-            # printf((l, r))
-            m = (l + r) >> 1
-            if(x <= b[m]):
-                r = m
-            else:
-                l = m + 1
-        
+        r = bisect.bisect_left(b, x)
         printf(-1) if r == n else printf(r+1) 
 
             
---
+++ codeforces/Codeforces Round #790 (Div. 4)/h.py
@@ -0,0 +1,47 @@
+import sys;input=lambda:sys.stdin.readline().strip("\r\n")
+import platform
+LOCAL = (platform.uname().node == 'AMO')
+# print(LOCAL)
+def printf(a):
+    if LOCAL:
+        print('>>>:', end=' ')
+    
+    if(isinstance(a, list)):
+        print(' '.join(map(str, a)))
+    else:
+        print(a)
+
+
+class Fenwick:
+    def __init__(self, n):
+        
+        self.n = n
+        self.bits = [0] * (n+1)
+    
+    def sum(self, i):
+        ret = 0
+        while i > 0:
+            ret += self.bits[i]
+            i -= i & -i
+        return ret
+    
+    def add(self, i, x):
+        while i <= self.n:
+            self.bits[i] += x
+            i += i & -i
+
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    fenwick = Fenwick(n)
+    res = 0
+    for i, x in enumerate(a):
+        res += fenwick.sum(x)
+        fenwick.add(x, 1)
+        printf(res)
+    # printf(res)
+
+
+        
