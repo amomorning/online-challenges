629385e5080d6624331aca4b981bf179ec805541
Yichen Mo <amomorning@gmail.com>
2022-06-15 11:34:07 +0200

    Add random dict

--- codeforces/799/h.py
+++ codeforces/799/h.py
@@ -1,30 +1,51 @@
+import collections, math, bisect, heapq, random, functools, itertools, copy
 import sys; input = lambda:sys.stdin.readline().strip("\r\n")
 import platform; LOCAL = (platform.uname().node == 'AMO')
+
+def printt(a):
+    if LOCAL:
+        printf(a)
 
 def printf(a):
     if LOCAL:
         print('>>>:', end=' ')
-    
     if(isinstance(a, list)):
         print(' '.join(map(str, a)))
     else:
         print(a)
+
+
+
+class RandomDict:
+    def __init__(self, func=lambda x: 0):
+        self.RANDOM = random.randint(0, 1<<32)
+        self.default = func
+        self.dict = {}
+    
+    def __getitem__(self, key):
+        k = self.RANDOM ^ key
+        if k not in self.dict:
+            self.dict[k] = self.default()
+        return self.dict[k]
+    
+    def __setitem__(self, key, value):
+        k = self.RANDOM ^ key
+        self.dict[k] = value
+
+    def keys(self):
+        return [self.RANDOM ^ i for i in self.dict]
 
 from math import inf
 
 for _ in range(int(input())):
     n = int(input())
     a = list(map(int, input().split()))
-    
-    mp = {}
+    mp = RandomDict(list)
     for i in range(n):
-        if a[i] in mp:
-            mp[a[i]].append(i)
-        else:
-            mp[a[i]] = [i]
+        mp[a[i]].append(i)
     
     ans = (-inf, -1, -1, -1)
-    for k in mp:
+    for k in mp.keys():
         s = (inf, -1)
         m = len(mp[k])
 
