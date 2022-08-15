bbc865fbb9d188b7eaee6a70d4ce221fb3ec276e
Yichen Mo <amomorning@gmail.com>
2022-08-15 14:36:45 +0200

    Update

--- codeforces/813/d.py
+++ codeforces/813/d.py
@@ -18,27 +18,17 @@ for _ in range(int(input())):
         continue
     
     sorted_a = sorted(list(zip(a, range(n))))
-    # print(sorted_a)
 
     ans = 0
     for i in range(n-1):
-
-        # 0, 0
         ref = sorted([a[j] for j in [i, i+1]])
-        d = min(min(a[i], a[i+1]), get_kth(k, sorted_a, ref)*2)
-        ans = max(d, ans)
-
-        # 0, 1 
-        d = min(a[i+1], get_kth(k-1, sorted_a, ref)*2)
-        ans = max(ans, d)
-        # 1, 0
-        d = min(a[i], get_kth(k-1, sorted_a, ref)*2)
-        ans = max(ans, d)
-
-        if k == 1: continue
-        
-        d = min(int(1e9), get_kth(k-2, sorted_a, ref) * 2)
-        ans = max(ans, d)
+        for p in range(2):
+            for q in range(2):
+                if k-p-q < 0: continue
+                x = int(1e9) if p else a[i]
+                y = int(1e9) if q else a[i+1]
+                d = min(min(x, y), get_kth(k-p-q, sorted_a, ref)*2)
+                ans = max(ans, d)
     print(ans)
 
 
