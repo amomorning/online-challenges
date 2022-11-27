3ecb94fd5c6748ea71ba1c8d6a9dfffaf97c0f45
Yichen Mo <amomorning@gmail.com>
2022-11-27 16:03:10 +0100

    Add ABC 279 F

--- atcoder/abc279/f.py
+++ atcoder/abc279/f.py
@@ -4,7 +4,7 @@ import platform; LOCAL = (platform.uname().node == 'AMO')
 
 import sys; input = lambda: sys.stdin.readline().rstrip("\r\n")
 inp = lambda f=int: list(map(f, input().split()))
-
+sys.setrecursionlimit(10**6)
 def debug(*args):
     if LOCAL:
         print('\033[92m', end='')
@@ -24,83 +24,41 @@ def printf(*args):
     print()
 
 class UnionFind:
-    def __init__(self, x) -> None:
-        self.uf = [-1] * x
- 
+    def __init__(self, n):
+        self.uf = list(range(n))
+        self.p = list(range(n))
+        self.id = list(range(n))
+    
     def find(self, x):
-        r = x
-        while self.uf[x] >= 0:
-            x = self.uf[x]
-        while r != x:
-            self.uf[r], r = x, self.uf[r]
-        return x
- 
-    def same(self, x, y):
-        return self.find(x) == self.find(y)
+        if self.uf[x] != x:
+            self.uf[x] = self.find(self.uf[x])
+        return self.uf[x]
     
-    def unionBy(self, x, y):
-        ux, uy = self.find(x), self.find(y)
-        if ux == uy: return False
-        self.uf[uy] = ux
-        return True
-
     def union(self, x, y):
-        ux, uy = self.find(x), self.find(y)
-        if ux == uy:
-            return False
-        if self.uf[ux] >= self.uf[uy]:
-            self.uf[uy] += self.uf[ux]
-            self.uf[ux] = uy
+        if self.id[y] == -1:
+            return
+        if self.id[x] == -1:
+            self.id[x] = self.id[y]
+            self.p[self.id[x]] = x
         else:
-            self.uf[ux] += self.uf[uy]
-            self.uf[uy] = ux
-        return True
- 
-    def count(self):
-        return sum(f < 0 for f in self.uf)
- 
-    def valid(self):
-        n = len(self.uf)
-        for c in range(n):
-            if self.uf[c] == -n:
-                return True
-        return False
+            self.uf[self.id[y]] = self.id[x]
+        self.id[y] = -1
     
-    def roots(self):
-        return [i for i, f in enumerate(self.uf) if f < 0]
-
-    def groups(self):
-        n = len(self.uf)
-        ret = [[] for _ in range(n)]
-        for i in range(n):
-            f = self.find(i)
-            ret[f].append(i)
-        return ret
- 
-    def __print__(self):
-        return self.uf
+    
 
 def solve(cas):
     n, q = inp()
-    uf = UnionFind(n+q)
     cur = n
+    uf = UnionFind(n+q)
     for _ in range(q):
-        op, *a = inp(lambda x: int(x)-1)
-        if op == 2:
-            print(uf.find(a[0]) + 1)
-        if op == 1:
-            uf.unionBy(a[0], cur)
+        op = inp(lambda x: int(x)-1)
+        if op[0] == 0:
+            uf.union(op[1], op[2])
+        if op[0] == 1:
+            uf.union(op[1], cur)
             cur += 1
-        if op == 0:
-            uf.unionBy(a[0], a[1])
-            debug('cur =', a[0], a[1])
-        for i in range(cur):
-            debug(i, uf.find(i))
-        debug('------------')
-
-
-        
-        
+        if op[0] == 2:
+            print(uf.p[uf.find(op[1])] + 1)
 
 def make_arr(*args):
     def func(x):
