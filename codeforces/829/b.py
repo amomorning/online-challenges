d32f22d54ab9191de5483be1d0967a6c74639d24
Yichen Mo <amomorning@gmail.com>
2023-01-04 15:32:39 +0100

    Add CF 829

---
+++ codeforces/829/b.py
@@ -0,0 +1,60 @@
+import collections, math, bisect, heapq, random, functools, itertools, copy, typing
+import platform; LOCAL = (platform.uname().node == 'AMO')
+
+
+import sys; input = lambda: sys.stdin.readline().rstrip("\r\n")
+inp = lambda f=int: list(map(f, input().split()))
+
+def make_arr(*args):
+    def func(x):
+        if len(args) == 1: return [x for _ in range(args[0])]
+        return [make_arr(*args[1:])(x) for _ in range(args[0])]
+    return func
+
+def debug(*args):
+    if LOCAL:
+        print('\033[92m', end='')
+        printf(*args)
+        print('\033[0m', end='')
+
+def printf(*args):
+    if LOCAL:
+        print('>>>: ', end='')
+    for arg in args:
+        if isinstance(arg, typing.Iterable) and \
+                not isinstance(arg, str) and \
+                not isinstance(arg, dict):
+            print(' '.join(map(str, arg)), end=' ')
+        else:
+            print(arg, end=' ')
+    print()
+
+# avaliable on Google, AtCoder
+# sys.setrecursionlimit(10**6)
+# import numpy as np
+# import scipy
+
+# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
+# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
+
+def solve(cas):
+    n, = inp()
+    ans = []
+    if n <= 3:
+        for i in range(n):
+            ans.append(i+1)
+    else:
+        tmp = n//2
+        for i in range(n//2, 0, -1):
+            ans.append(i)
+            ans.append(i+tmp)
+        if n % 2 > 0:
+            ans.append(n)
+    printf(ans)
+
+
+cas = 1
+cas = int(input())
+for _ in range(cas):
+    solve(cas)
