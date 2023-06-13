2d3a01db01e8db0881fbc8e99375c72ef1486bd1
Yichen Mo <amomorning@gmail.com>
2023-06-13 17:21:59 +0800

    Update

--- codeforces/edu150/c_dp.py
+++ codeforces/edu150/c_dp.py
@@ -38,40 +38,23 @@ def printf(*args):
 # d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
 # d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
 
+MX = 5
+
 def solve(cas):
     s = list(map(lambda c: ord(c) - ord('A'), list(input())))
     n = len(s)
-    F = make_arr(5, 2)(lambda:-math.inf) # 0 - non 1 - modify
+    F = make_arr(MX, 2)(lambda:-math.inf) # 0 - non 1 - modify
     F[0][0] = 0
     for i in range(n-1, -1, -1):
-        G = make_arr(5, 2)(lambda:-math.inf)
-        for j in range(5):
-            # F[j][1]
-            if s[i] < j:
-                G[j][1] = max(G[j][1], F[j][1] - 10**s[i])
-            else:
-                G[s[i]][1] = max(G[s[i]][1], F[j][1] + 10**s[i])
-            
-            # F[j][0]
-            for k in range(5):
-                if k == s[i]:
-                    if k < j:
-                        G[j][0] = max(G[j][0], F[j][0] - 10**k)
-                    else:
-                        G[k][0] = max(G[k][0], F[j][0] + 10**k)
-                else:
-                    if k < j:
-                        G[j][1] = max(G[j][1], F[j][0] - 10**k)
-                    else:
-                        G[k][1] = max(G[k][1], F[j][0] + 10**k)
+        G = make_arr(MX, 2)(lambda:-math.inf)
+        for j in range(MX):
+            G[max(j, s[i])][1] = max(G[max(j, s[i])][1], F[j][1] + (-1)**(s[i]<j) * 10**s[i])
+            for k in range(MX):
+                G[max(j, k)][k!=s[i]] = max(G[max(j, k)][k!=s[i]], F[j][0] + (-1)**(k<j) * 10**k)
 
         F = G
-    ans = -math.inf
-    for i in range(5):
-        for j in range(2):
-            ans = max(ans, F[i][j])
-                
-    print(ans)
+    
+    print(max(map(max, F)))
                         
                 
 
