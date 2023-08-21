cbc7bec094cedb34cc91340c22965b3d19011a36
Yichen Mo <amomorning@gmail.com>
2023-08-21 20:03:54 +0800

    Add ABC 313

---
+++ atcoder/abc313/b.py
@@ -0,0 +1,59 @@
+import collections, math, bisect, heapq, random, functools, itertools, copy, typing
+import platform; LOCAL = (platform.uname().node == 'AMO')
+
+
+import sys; input = lambda: sys.stdin.readline().rstrip("\r\n")
+inp = lambda f=int: list(map(f, input().split()))
+
+def make_arr(*args):
+    def func(x):
+        if len(args) == 1: return [x() for _ in range(args[0])]
+        return [make_arr(*args[1:])(x) for _ in range(args[0])]
+    return func
+
+def debug(*args):
+    if LOCAL:
+        print('\033[92m', end='')
+        printf(*args)
+        print('\033[0m', end='')
+
+def printf(*args):
+    if LOCAL:
+        print('>>>: ', end='')
+    for arg in args:
+        if isinstance(arg, typing.Iterable) and \
+                not isinstance(arg, str) and \
+                not isinstance(arg, dict):
+            print(' '.join(map(str, arg)), end=' ')
+        else:
+            print(arg, end=' ')
+    print()
+
+# avaliable on Google, AtCoder
+# sys.setrecursionlimit(10**6)
+# import numpy as np
+# import scipy
+
+# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
+# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
+
+def solve(cas):
+    n, m = inp()
+    mx = [-1] * n
+    for i in range(m):
+        u, v = inp(lambda x: int(x)-1)
+        if mx[u] == -1:
+            mx[v] = u
+        else:
+            mx[v] = mx[u]
+    if mx.count(-1) == 1:
+        print(mx.index(-1)+ 1)
+    else:
+        print(-1)
+
+
+cas = 1
+for _ in range(cas):
+    solve(_)
+
