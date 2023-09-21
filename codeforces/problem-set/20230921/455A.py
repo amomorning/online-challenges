8a940679d8ada3c22a202b6b517660e4b72c3701
Yichen Mo <amomorning@gmail.com>
2023-09-21 23:21:45 +0800

    Add daily training

---
+++ codeforces/problem-set/20230921/455A.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import collections, math, bisect, heapq, random, functools, itertools, copy, typing
+import platform; LOCAL = (platform.uname().node == 'AMO')
+
+
+import sys; input = lambda: sys.stdin.readline().rstrip("\r\n")
+inp = lambda f=int: list(map(f, input().split()))
+
+def make_arr(*args):
+    def func(x):
+        if len(args) == 1: return [x() for _ in range(args[0])]
+        return [make_arr(*args[1:])(x) for _ in range(args[0])]
+    return func
+
+def debug(*args):
+    if LOCAL:
+        print('\033[92m', end='')
+        printf(*args)
+        print('\033[0m', end='')
+
+def printf(*args):
+    if LOCAL:
+        print('>>>: ', end='')
+    for arg in args:
+        if isinstance(arg, typing.Iterable) and \
+                not isinstance(arg, str) and \
+                not isinstance(arg, dict):
+            print(' '.join(map(str, arg)), end=' ')
+        else:
+            print(arg, end=' ')
+    print()
+
+
+# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
+# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
+def solve(cas):
+    n, = inp()
+    MAX = int(1e5)+5
+    cnt = [0] * MAX
+    a = inp()
+    for c in a:
+        cnt[c] += 1
+    dp = [0] * MAX
+    dp[1] = cnt[1]
+    for i in range(2, MAX):
+        dp[i] = max(dp[i-1], dp[i-2] + cnt[i]*i)
+    print(dp[MAX-1])
+
+cas = 1
+for _ in range(cas):
+    solve(_)
+
---
+++ codeforces/problem-set/20230921/472A.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import collections, math, bisect, heapq, random, functools, itertools, copy, typing
+import platform; LOCAL = (platform.uname().node == 'AMO')
+
+
+import sys; input = lambda: sys.stdin.readline().rstrip("\r\n")
+inp = lambda f=int: list(map(f, input().split()))
+
+def make_arr(*args):
+    def func(x):
+        if len(args) == 1: return [x() for _ in range(args[0])]
+        return [make_arr(*args[1:])(x) for _ in range(args[0])]
+    return func
+
+def debug(*args):
+    if LOCAL:
+        print('\033[92m', end='')
+        printf(*args)
+        print('\033[0m', end='')
+
+def printf(*args):
+    if LOCAL:
+        print('>>>: ', end='')
+    for arg in args:
+        if isinstance(arg, typing.Iterable) and \
+                not isinstance(arg, str) and \
+                not isinstance(arg, dict):
+            print(' '.join(map(str, arg)), end=' ')
+        else:
+            print(arg, end=' ')
+    print()
+
+
+# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
+# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
+
+
+def is_prime(x):    
+    if x == 1: return False
+    if x == 2: return True
+    for i in range(2, int(math.sqrt(x))+1):
+        if x % i == 0: return False
+    return True
+    
+
+
+def solve(cas):
+    n, = inp()
+    for i in range(3, n):
+        if is_prime(i) or is_prime(n-i):
+            continue
+        print(i, n-i)
+        return
+
+
+cas = 1
+for _ in range(cas):
+    solve(_)
+
