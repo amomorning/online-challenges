8a1586d45a80b53793571c5824ebfd794b4fda0b
Yichen Mo <amomorning@gmail.com>
2024-06-30 22:45:42 +0800

    Add ABC360E

---
+++ atcoder/abc360/e.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+import collections, math, bisect, heapq, random, functools, itertools, copy, typing
+import platform; LOCAL = (platform.uname().node == 'AMO')
+
+
+import sys; input = lambda: sys.stdin.readline().rstrip("\r\n")
+inp = lambda f=int: list(map(f, input().split()))
+
+def make_arr(*args):
+    def func(x):
+        if len(args) == 1: return [x() for _ in range(args[0])]
+        return [make_arr(*args[1:])(x) for _ in range(args[0])]
+    return func
+
+def debug(*args):
+    if LOCAL:
+        print('\033[92m', end='')
+        printf(*args)
+        print('\033[0m', end='')
+
+def printf(*args):
+    if LOCAL:
+        print('>>>: ', end='')
+    for arg in args:
+        if isinstance(arg, typing.Iterable) and \
+                not isinstance(arg, str) and \
+                not isinstance(arg, dict):
+            print(' '.join(map(str, arg)), end=' ')
+        else:
+            print(arg, end=' ')
+    print()
+
+
+# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
+# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
+
+MOD = 998244353
+def norm(v):
+    v = v % MOD
+    if v < 0: v += MOD
+    return v
+
+class Integral:
+    def __init__(self, v):
+        self.v = norm(v)
+
+    def __str__(self):
+        return str(self.v)
+    
+    def __neg__(self):
+        return Integral(-self.v)
+
+    def __iadd__(self, rhs):
+        if type(rhs) == Integral: rhs = rhs.v
+        self.v = norm(self.v + rhs)
+        return self
+    
+    def __isub__(self, rhs):
+        self += -rhs
+        return self
+        
+    def __imul__(self, rhs):
+        if type(rhs) == int: rhs = Integral(rhs)
+        self.v = norm(self.v * rhs.v)
+        return self
+    
+    def __itruediv__(self, rhs):
+        if type(rhs) == int: rhs = Integral(rhs)
+        self.v = norm(self.v * rhs.inv().v)
+        return self
+    
+    def copy(self):
+        return Integral(self.v)
+    
+    def pow(self, b):
+        assert type(b) == int
+        ret, a = 1, self.v
+        while b > 0:
+            if b & 1:
+                ret = ret * a % MOD
+            b >>= 1
+            a = a * a % MOD
+        return Integral(ret)
+    
+    def inv(self):
+        return self.pow(MOD-2)
+    
+    def __add__(self, rhs):
+        ret = self.copy(); ret += rhs
+        return ret
+
+    def __sub__(self, rhs):
+        ret = self.copy(); ret -= rhs
+        return ret
+    
+    def __mul__(self, rhs):
+        ret = self.copy(); ret *= rhs
+        return ret
+    
+    def __truediv__(self, rhs):
+        ret = self.copy(); ret /= rhs
+        return ret
+    
+    def __lshift__(self, rhs):
+        if type(rhs) == Integral: rhs = rhs.v
+        return Integral(self.v << rhs)
+
+    def __rshift__(self, rhs):
+        if type(rhs) == Integral: rhs = rhs.v
+        return Integral(self.v >> rhs)
+    
+    def __pow__(self, rhs):
+        return self.pow(rhs)
+    
+    def __eq__(self, rhs):
+        if type(rhs) == Integral: rhs = rhs.v
+        return self.v == rhs
+    
+    def __lt__(self, rhs):
+        if type(rhs) == Integral: rhs = rhs.v
+        return self.v < rhs
+    
+    def __le__(self, rhs):
+        if type(rhs) == Integral: rhs = rhs.v
+        return self.v <= rhs
+
+
+
+def solve(cas):
+    n, k = inp()
+    p = Integral(2*(n-1))/Integral(n**2)
+    q = Integral(2)/Integral(n**2)
+    d = Integral(1)
+    for i in range(k):
+        d = d - d*(p+q) + q
+    u = Integral((n+2))/2
+    print(d + u - u * d)
+
+
+        
+
+cas = 1
+for _ in range(cas):
+    solve(_)
+
